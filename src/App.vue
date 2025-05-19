2a70b7b2e9e956c8c033ea2db17ebd0ff8b6d857
ketrololz <bqnexxx@gmail.com>
2025-05-19 15:28:34 +0300

    feat: implement user list loading and remove user button

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,35 @@ import { Form } from '@primevue/forms';
 import InputText from 'primevue/inputtext';
 import Password from 'primevue/password';
 import Select from 'primevue/select';
-import { useUserStore } from './state/user-store';
+import { useUserStore, type MarkText, type User } from './state/user-store';
+import { markRaw, ref } from 'vue';
 
 const store = useUserStore();
+const accountType = ref([
+  {
+    name: 'Локальная',
+    type: 'Local',
+  },
+  {
+    name: 'LDAP',
+    type: 'LDAP',
+  },
+]);
 
+const testUser: User = {
+  id: 1,
+  login: 'test',
+  type: 'Local',
+  password: '12345',
+  marks: [{ text: 'test' }, { text: 'test2' }]
+}
+
+function convertMarks(marks: MarkText[]) {
+  const result = marks.reduce((acc, mark) => acc.concat(mark.text, '; '), '');
+  return result.trim().slice(0, -1);
+}
+
+store.addUser(testUser)
 </script>
 
 <template>
@@ -17,32 +42,42 @@ const store = useUserStore();
         <h1 class="text-2xl font-semibold">Учётные записи</h1>
         <Button icon="pi pi-plus" variant="outlined"></Button>
       </div>
-      <div class="bg-(--p-button-secondary-background) px-4 py-3 rounded-xl flex items-center gap-x-2">
+      <div
+        class="bg-(--p-button-secondary-background) px-4 py-3 rounded-xl flex items-center gap-x-2"
+      >
         <i class="pi pi-info-circle" style="font-size: 1.2rem"></i>
         Для указания нескольких меток для одной пары логин/пароль используйте
         разделитель <span class="font-bold text-green-600">;</span>
       </div>
     </div>
-    <Form v-slot="$form" class="flex flex-col gap-4 w-full pt-4">
+    <Form
+      v-for="user of store.userList"
+      v-bind:key="user.id"
+      v-slot="$form"
+      class="flex flex-col gap-4 w-full pt-4"
+    >
       <div class="flex gap-2">
         <InputText
           name="mark"
           type="text"
           placeholder="XXX; YYY"
           class="min-w-40"
+          :value="convertMarks(user.marks)"
           fluid
         />
         <Select
-          v-model="selectedPasswordType"
-          :options="passwordType"
-          optionLabel="type"
-          placeholder="Локальная"
+          v-model="user.type"
+          :options="accountType"
+          optionLabel="name"
+          optionValue="type"
+          class="min-w-40"
         />
         <div class="flex gap-2 w-full">
           <InputText
             name="login"
             type="text"
             placeholder="Логин"
+            :value="user.login"
             class="flex-1"
             fluid
           />
@@ -51,6 +86,7 @@ const store = useUserStore();
             type="password"
             placeholder="Пароль"
             toggle-mask
+            v-model="user.password"
             :feedback="false"
             class="flex-1"
             fluid
@@ -63,7 +99,7 @@ const store = useUserStore();
           variant="simple"
           >{{ $form.username.error?.message }}</Message
         >
-        <Button severity="secondary" icon="pi pi-trash" class="min-w-10" />
+        <Button severity="secondary" icon="pi pi-trash" class="min-w-10" @click="store.removeUser(user.id)" />
       </div>
     </Form>
   </div>
--- src/state/user-store.ts
+++ src/state/user-store.ts
@@ -1,29 +1,33 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
 export type MarkText = {
   text: string;
-}
+};
 
 export interface User {
-  id: number
-  marks: MarkText[]
-  type: 'LDAP' | 'Local'
-  login: string
-  password: string | null
+  id: number;
+  marks: MarkText[];
+  type: 'LDAP' | 'Local';
+  login: string;
+  password: string | null;
 }
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    userList: [] as User[]
+    userList: [] as User[],
   }),
   getters: {
     getUserById: (state) => {
-      return (userId: number) => state.userList.find((user) => user.id === userId)
+      return (userId: number) =>
+        state.userList.find((user) => user.id === userId);
     },
   },
   actions: {
     addUser(user: User) {
-      this.userList.push(user)
+      this.userList.push(user);
+    },
+    removeUser(userId: number) {
+      this.userList = this.userList.filter((user) => user.id !== userId);
     },
   },
-})
+});
